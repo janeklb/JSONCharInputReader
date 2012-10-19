7d9e0e09a0bf338420b476d562b18845e75f7cdf
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2012-10-19 16:55:30 +0100

    moved to PHPUnit

--- test/test.php
+++ test/test.php
@@ -1,22 +1,48 @@
 <?php
 
+//////////////////////////////////////////////////////////////////////////////
+//                                                                          //
+// Note:                                                                    //
+// Requires PHPUnit                                                         //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
+
+
+require_once __DIR__ . '/../JSONChunkProcessor.php';
+require_once __DIR__ . '/../JSONCharInputReader.php';
+
+class JSONChunkProcessorImpl implements JSONChunkProcessor
+{
+	private $objects = array();
+
+	public function process($jsonChunk)
+	{
+		$this->objects[] = json_decode($jsonChunk);
+	}
+
+	public function getObjects()
+	{
+		return $this->objects;
+	}
+}
+
 /**
  * A few very simple simpletests
  * @author janeklb
  *
  */
-class JSONCarInputReaderTest extends UnitTestCase
+class JSONCarInputReaderTest extends PHPUnit_Framework_TestCase
 {
 	private $processor;
 	private $inputReader;
-	
+
 	public function setUp()
 	{
 		$this->processor = new JSONChunkProcessorImpl();
 		$this->inputReader = new JSONCharInputReader($this->processor);
 		$this->inputReader->readChar('[');
 	}
-	
+
 	private function sendInput($string)
 	{
 		$len = strlen($string);
@@ -25,57 +51,57 @@ class JSONCarInputReaderTest extends UnitTestCase
 			$this->inputReader->readChar($string[$i]);
 		}
 	}
-	
+
 	private function assertObjects()
 	{
 		$objects = $this->processor->getObjects();
-		
-		$this->assertEqual(count($objects), func_num_args());
-		
+
+		$this->assertEquals(count($objects), func_num_args());
+
 		foreach ($objects as $i => $object)
 		{
-			$this->assertEqual($object, func_get_arg($i));
+			$this->assertEquals($object, func_get_arg($i));
 		}
 	}
-	
+
     public function testSingleInteger() {
-    	
+
     	$this->sendInput('1');
     	$this->assertObjects();
-    	
+
     	$this->sendInput(',');
     	$this->assertObjects(1);
     }
-    
+
     public function testArray()
     {
     	$this->sendInput('[232,2412]');
     	$this->assertObjects(array(232, 2412));
     }
-    
+
     public function testObject()
     {
     	$this->sendInput('{"x": "hello"}');
-    	
+
     	$test = new stdClass();
     	$test->x = "hello";
     	$this->assertObjects($test);
     }
-    
+
     public function testMixed()
     {
     	$this->sendInput('2, 3, 4, [1, {"y": [1, {"b": "x"}, 3], "o" : 2}, [4, 2]], {"ob": "bo"}');
-    	
+
     	$objA = new stdClass();
     	$objA->b = "x";
-    	
+
     	$objB = new stdClass();
     	$objB->y = array(1, $objA, 3);
     	$objB->o = 2;
-    	
+
     	$objC = new stdClass();
     	$objC->ob = "bo";
-    	
+
     	$this->assertObjects(2, 3, 4, array(1, $objB, array(4, 2)), $objC);
     }
 }
